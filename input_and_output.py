b2c784a102c3acfeda0b452b93e364649bfa72bf
IGassan <igasan96@gmail.com>
2022-08-17 14:34:19 +0300

    Ввод вывод данных

---
+++ input_and_output.py
@@ -0,0 +1,45 @@
+print("Здравствуй, мир!")
+
+print(4, 8, 15, 16, 23, 42)
+
+print(4)
+print(8)
+print(15)
+print(16)
+print(23)
+print(42)
+
+for i in range(1,8):
+    print("*" * i)
+
+name = input()
+print('Привет,', name)
+
+name = input()
+print(name, '- чемпион!')
+
+x = input()
+y = input()
+z = input()
+print(x)
+print(y)
+print(z)
+
+x = input()
+y = input()
+z = input()
+print(z)
+print(y)
+print(x)
+
+print('I', 'like', 'Python', sep='***')
+
+a = input()
+x = input()
+y = input()
+z = input()
+print(x, y, z, sep=a)
+
+name = input()
+print('Привет,', name, end='')
+print('!')
