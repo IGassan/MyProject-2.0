4e254e7dbccf7910683155b87e25a84e2cf2cd94
IGassan <igasan96@gmail.com>
2022-09-08 16:04:29 +0300

    Продолжение изучения функций, итоговая работа по спискам

--- functions.py
+++ functions.py
@@ -288,3 +288,121 @@ def convert_to_python_case(text):
 txt = input()
 
 print(convert_to_python_case(txt))
+
+
+def get_middle_point(x1, y1, x2, y2):
+    return (x1 + x2) / 2, (y1 + y2) / 2
+
+x_1, y_1 = int(input()), int(input())
+x_2, y_2 = int(input()), int(input())
+
+x, y = get_middle_point(x_1, y_1, x_2, y_2)
+print(x, y)
+
+
+from math import pi
+
+def get_circle(radius):
+    return 2 * pi * radius, pi * radius ** 2
+
+r = float(input())
+
+length, square = get_circle(r)
+print(length, square)
+
+from math import *
+
+def solve(a, b, c):
+    D = pow(b, 2) - 4 * a * c
+    x1 = (-b - sqrt(D)) / (2 * a)
+    x2 = (-b + sqrt(D)) / (2 * a)
+    if x1 < x2:
+        return x1, x2
+    else:
+        return x2, x1
+
+a, b, c = int(input()), int(input()), int(input())
+
+x1, x2 = solve(a, b, c)
+print(x1, x2)
+
+
+# Итоговая работа
+
+
+def draw_triangle():
+    for i in range (1, 9):
+        print((' ' * (8 - i)) + ('*' * (i * 2 - 1)))
+
+draw_triangle()  
+
+
+def get_shipping_cost(quantity):
+    if quantity > 1:
+        return (quantity - 1) * 120 + 1000
+    else:
+        return 1000
+
+n = int(input())
+
+print(get_shipping_cost(n))
+
+from math import factorial
+
+def compute_binom(n, k):
+    return int((factorial(n)) / (factorial(k) * factorial(n - k)))
+
+n = int(input())
+k = int(input())
+
+print(compute_binom(n, k))
+
+
+def number_to_words(num):
+    s = ['один', 'два', 'три', 'четыре', 'пять', 'шесть', 'семь', 'восемь', 'девять', 'десять', 'одиннадцать', 'двенадцать', 'тринадцать', 'четырнадцать', 'пятнадцать', 'шестнадцать', 'семнадцать', 'восемнадцать', 'девятнадцать', 'двадцать', 'тридцать', 'сорок', 'пятьдесят', 'шестьдесят', 'семьдесят', 'восемьдесят', 'девяносто','']
+    if num <= 20:
+        return s[num - 1]
+    else:
+        return s[num // 10 - 1 + 18] + ' ' + s[num % 10 - 1]
+
+n = int(input())
+
+print(number_to_words(n))
+
+
+def get_month(language, number):
+    lng_ru = ['январь', 'февраль', 'март', 'апрель', 'май', 'июнь', 'июль', 'август', 'сентябрь', 'октябрь', 'ноябрь', 'декабрь']
+    lng_en = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december']
+    if language == 'ru':
+        return lng_ru[number - 1]
+    else:
+        return lng_en[number - 1]
+
+lan = input()
+num = int(input())
+
+print(get_month(lan, num))
+
+
+def is_magic(date):
+    date = date.split('.')
+    if int(date[0]) * int(date[1]) == int(date[2][2:5]):
+        return True
+    else:
+        return False
+
+date = input()
+
+print(is_magic(date))
+
+
+def is_pangram(text):
+    abc = "abcdefghijklmnopqrstuvwxyz"
+    for i in abc:
+        if i not in text.lower():
+            return False
+    return True
+
+text = input()
+
+print(is_pangram(text))
