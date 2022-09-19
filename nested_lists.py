e8003b5ef319b44651fdfafc46c1a0225e817902
IGassan <igasan96@gmail.com>
2022-09-19 16:23:36 +0300

    Продолжение изучения вложенных списков

--- nested_lists.py
+++ nested_lists.py
@@ -50,6 +50,7 @@ for i in range(1,a + 1):
 
 
 from math import *
+from re import M
 
 a = int(input())
 lists = []
@@ -90,3 +91,107 @@ for char in input().split():
 if a:
     char_lists.append(a)
 print(char_lists)
+
+
+a = input().split()
+n = int(input())
+total = list()
+for i in range(0, len(a), n):
+    total.append(a[i: i + n])
+print(total)
+
+
+input_data = input().split()
+output_data = [[]]
+for i in range(len(input_data)):
+    for j in range(len(input_data) - i):
+        output_data.append(input_data[j: j + i + 1])
+print(output_data)
+
+n = int(input())
+m = int(input())
+matrix = [[0] * m for _ in range(n)]
+for i in range(n):
+    for  j in range(m):
+        matrix[i][j] = input()
+for i in range(n):
+    for j in range(m):
+        print(matrix[i][j], end=' ')
+    print()
+
+
+n = int(input())
+m = int(input())
+matrix = [[0] * m for _ in range(n)]
+for i in range(n):
+    for  j in range(m):
+        matrix[i][j] = input()
+for i in range(n):
+    for j in range(m):
+        print(matrix[i][j], end=' ')
+    print()
+print()
+for i in range(m):
+    for j in range(n):
+        print(matrix[j][i], end=' ')
+    print()
+
+
+matrix = [input().split() for _ in range(int(input()))]
+total = 0
+for i in range(len(matrix)):
+    for  j in range(len(matrix[i])):
+        if i == j:
+            total += int(matrix[i][j])
+print(total)
+
+
+matrix = [list(map(int, input().split())) for _ in range(int(input()))]
+total = 0
+for i in range(len(matrix)):
+    for  j in range(len(matrix[i])):
+        if matrix[i][j] > sum(matrix[i]) / len(matrix[i]):
+            total += 1
+    print(total)
+    total = 0
+
+
+matrix = [list(map(int, input().split())) for _ in range(int(input()))]
+total = -10000
+for i in range(len(matrix)):
+    for  j in range(len(matrix[i])):
+        if i >=j:
+            if matrix[i][j] > total:
+                total = matrix[i][j]
+print(total)
+
+
+matrix = [list(map(int, input().split())) for _ in range(int(input()))]
+total = -1000
+for i in range(len(matrix)):
+    for  j in range(len(matrix[i])):
+        if  (j <= i <= len(matrix) - 1 - j) or len(matrix) - 1 - j <= i <= j:
+            if matrix[i][j] > total:
+                total = matrix[i][j]
+print(total)
+
+
+matrix = [list(map(int, input().split())) for _ in range(int(input()))]
+total1 = 0
+total2 = 0
+total3 = 0
+total4 = 0
+for i in range(len(matrix)):
+    for  j in range(len(matrix[i])):
+        if  i < j and i < len(matrix) - 1 - j:
+            total1 += matrix[i][j]
+        if  i < j and i > len(matrix) - 1 - j:
+            total2 += matrix[i][j]
+        if  i > j and i > len(matrix) - 1 - j:
+            total3 += matrix[i][j]
+        if  i > j and i < len(matrix) - 1 - j:
+            total4 += matrix[i][j]
+print('Верхняя четверть:', total1)
+print('Правая четверть:', total2)
+print('Нижняя четверть:', total3)
+print('Левая четверть:', total4)
