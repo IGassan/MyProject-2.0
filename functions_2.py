a81e5dde101308294066f693ac63a8d09d8aeb91
IGassan <igasan96@gmail.com>
2022-12-12 17:57:09 +0300

    Продолжение изучения функций

--- functions_2.py
+++ functions_2.py
@@ -276,3 +276,26 @@ data = [['Tokyo', 35676000, 'primary'],
 total = list(filter(lambda x: x[1] > 10000000 and x[2] == 'primary', data))
 total = list(sorted(total))
 print(reduce(lambda x, y: f'{x} {y[0]},', total, 'Cities:').strip(','))
+
+
+func = lambda x: x % 19 == 0 or x % 13 == 0 
+
+
+func = lambda x: x[0].upper() == 'A' and x[-1].upper() == 'A'
+
+
+is_non_negative_num = lambda x: x.replace('.', '').isdigit() and x.count('.') < 2
+
+
+is_num = lambda x: '-' not in x[1:] and x.replace('.', '', 1).replace('-', '').isdigit()
+
+
+words = ['beverage', 'monday', 'abroad', 'bias', 'abuse', 'abolish', 'abuse', 'abuse', 'bid', 'wednesday', 'able', 'betray', 'accident', 'abduct', 'bigot', 'bet', 'abandon', 'besides', 'access', 'friday', 'bestow', 'abound', 'absent', 'beware', 'abundant', 'abnormal', 'aboard', 'about', 'accelerate', 'abort', 'thursday', 'tuesday', 'sunday', 'berth', 'beyond', 'benevolent', 'abate', 'abide', 'bicycle', 'beside', 'accept', 'berry', 'bewilder', 'abrupt', 'saturday', 'accessory', 'absorb']
+total = list(filter(lambda x: len(x) == 6, words))
+print(*sorted(total))
+
+
+numbers = [46, 61, 34, 17, 56, 26, 93, 1, 3, 82, 71, 37, 80, 27, 77, 94, 34, 100, 36, 81, 33, 81, 66, 83, 41, 80, 80, 93, 40, 34, 32, 16, 5, 16, 40, 93, 36, 65, 8, 19, 8, 75, 66, 21, 72, 32, 41, 59, 35, 64, 49, 78, 83, 27, 57, 53, 43, 35, 48, 17, 19, 40, 90, 57, 77, 56, 80, 95, 90, 27, 26, 6, 4, 23, 52, 39, 63, 74, 15, 66, 29, 88, 94, 37, 44, 2, 38, 36, 32, 49, 5, 33, 60, 94, 89, 8, 36, 94, 46, 33]
+total = list(filter(lambda x: not(x % 2 and x > 47), numbers))
+total = list(map(lambda x: x // 2 if x % 2 == 0 else x, total))
+print(*total)
