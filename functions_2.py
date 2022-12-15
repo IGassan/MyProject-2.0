dc1333c1e938859f25ae5b8335dc43e665858c73
IGassan <igasan96@gmail.com>
2022-12-15 16:56:46 +0300

    Продолжение изучения функций

--- functions_2.py
+++ functions_2.py
@@ -330,3 +330,21 @@ from functools import reduce
 
 evaluate = lambda coefficients, x: reduce(lambda s, a: s * x + a, coefficients, 0)
 print(evaluate([*map(int, input().split())], int(input())))
+
+
+def ignore_command(command):
+    ignore = ['alias', 'configuration', 'ip', 'sql', 'select', 'update', 'exec', 'del', 'truncate']
+    return any(map(lambda x: x in command, ignore))
+
+
+countries = ['Russia', 'USA', 'UK', 'Germany', 'France', 'India']
+capitals = ['Moscow', 'Washington', 'London', 'Berlin', 'Paris', 'Delhi']
+population = [145_934_462, 331_002_651, 80_345_321, 67_886_011, 65_273_511, 1_380_004_385]
+for country, capital, populat in zip(countries, capitals, population):
+    print(f'{capital} is the capital of {country}, population equal {populat} people.')
+
+
+abscissas = list(map(float, input().split()))
+ordinates = list(map(float, input().split()))
+applicates = list(map(float, input().split()))
+print(all(list(map(lambda x,y,z: x**2+y**2+z**2<=4, abscissas,ordinates,applicates))))
