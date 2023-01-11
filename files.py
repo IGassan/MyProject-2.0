77c96123166454968c5701b4f817d39dd5b8eae2
IGassan <igasan96@gmail.com>
2023-01-11 17:51:29 +0300

    Продолжение изучения файлов

--- files.py
+++ files.py
@@ -32,3 +32,55 @@ file = open('prices.txt')
 lines = map(str.split, file)
 print(sum(map(lambda line: int(line[1]) * int(line[2]), lines)))
 file.close()
+
+
+with open('text.txt', encoding='utf-8') as file:
+    line = file.read()
+    print(line[::-1])
+
+
+with open('data.txt', encoding='utf-8') as file:
+    lines = file.readlines()[::-1]
+    for line in lines:
+        print(line, end='')
+
+
+with open('lines.txt') as f:
+    lines = [line.strip() for line in f.readlines()]
+    longest = max(map(len, lines))
+    print(*filter(lambda x: len(x) == longest, lines), sep='\n')
+
+
+with open('numbers.txt') as f:
+    for line in f:
+        print(sum(map(int, line.split())))
+
+
+with open('nums.txt') as f:
+    num = ''
+    lst = []
+    for s in f.read():
+        if s.isdigit():
+            num += s
+        else:
+            if num:
+                lst.append(int(num))
+                num = ''
+    print(sum(lst))
+
+
+with open('lines.txt') as f:
+    txt = f.read()
+    print('Input file contains:')
+    print(sum(map(str.isalpha, txt)), 'letters')
+    print(len(txt.split()), 'words')
+    print(txt.count('\n') + 1, 'lines')
+
+
+import random
+with open('first_names.txt') as names, open('last_names.txt') as surnames:
+    name = names.read().split()
+    surname = surnames.read().split()
+    print(f'{name[random.randint(1, len(name))]} {surname[random.randint(1, len(surname))]}')
+    print(f'{name[random.randint(1, len(name))]} {surname[random.randint(1, len(surname))]}')
+    print(f'{name[random.randint(1, len(name))]} {surname[random.randint(1, len(surname))]}')
